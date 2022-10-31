38efaa132a802cbb2c520e4117dc99da0203b7cb
kosuke <akaza586933@gmail.com>
2022-11-01 04:20:54 +0900

    leftバーにもログインユーザの情報が表示されるようにする

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -12,18 +12,19 @@ import Messages from "../../assets/10.png";
 import Tutorials from "../../assets/11.png";
 import Courses from "../../assets/12.png";
 import Fund from "../../assets/13.png";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
 
 function LeftBar() {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div className="leftBar">
       <div className="container">
         <div className="menu">
           <div className="user">
-            <img
-              src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <span>John Doe</span>
+            <img src={currentUser.profilePic} alt="" />
+            <span>{currentUser.name}</span>
           </div>
           <div className="item">
             <img src={Friends} alt="" />
@@ -46,7 +47,7 @@ function LeftBar() {
             <span>Memories</span>
           </div>
         </div>
-          <hr/>
+        <hr />
         <div className="menu">
           <span>Your shortcuts</span>
           <div className="item">
@@ -70,7 +71,7 @@ function LeftBar() {
             <span>Messages</span>
           </div>
         </div>
-          <hr/>
+        <hr />
         <div className="menu">
           <span>Others</span>
           <div className="item">
