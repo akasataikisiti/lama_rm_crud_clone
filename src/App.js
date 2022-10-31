c0b4748ef340622f2af99565130d3093861d3d98
kosuke <akaza586933@gmail.com>
2022-11-01 02:26:56 +0900

    navbarにダークモードを反映する
    切り替えよう関数をsrc/style.cssに作成
    これを使用して各コンポーネントのダーク、ライトモードを反映させる
    App.jsのLayoutコンポーネントのトップのdivクラス名でモード選択できる
      theme-dark => ダークモード、theme-light => ライトモード
    となる。

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import "./style.scss";
 import {
   createBrowserRouter,
   RouterProvider,
@@ -18,7 +19,7 @@ function App() {
 
   const Layout = () => {
     return (
-      <div>
+      <div className="theme-dark">
         <Navbar />
         <div style={{ display: "flex" }}>
           <LeftBar />
