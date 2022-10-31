b3d438a28ef2761588c69dd178cbb41d1f329b6b
kosuke <akaza586933@gmail.com>
2022-11-01 04:15:06 +0900

    仮ユーザでログインできるようにする
    authContextのlogin関数に仮置きのユーザ情報でログインできるように値を設定
    Login.jsのログインボタンでauthContextのlogin関数を実行するようにする。
    navBarにログインユーザの情報が表示されるようにする

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -9,10 +9,12 @@ import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { Link } from "react-router-dom";
-import { DarkModeContext, toggle } from "../../context/darkModeContext";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { AuthContext } from "../../context/authContext";
 
 function Navbar() {
   const { toggle, darkMode } = useContext(DarkModeContext);
+  const { currentUser } = useContext(AuthContext);
 
   return (
     <div className="navbar">
@@ -37,11 +39,8 @@ function Navbar() {
         <EmailOutlinedIcon />
         <NotificationsOutlinedIcon />
         <div className="user">
-          <img
-            src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
-            alt=""
-          />
-          <span>John Doe</span>
+          <img src={currentUser.profilePic} alt="" />
+          <span>{currentUser.name}</span>
         </div>
       </div>
     </div>
